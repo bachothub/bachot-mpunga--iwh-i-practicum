daaa1de2726416e995fcff460fcb76703a21db13
bachot mpunga bashala <bachotmpungabashala@bachots-MacBook-Air.local>
2024-10-21 14:17:42 +0400

    Initial commit

--- index.js
+++ index.js
@@ -1,32 +1,154 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
+// Express.js Middleware
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// Use the HubSpot Private App Access Token from environment variables
+const PRIVATE_APP_ACCESS = process.env.hubspot;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// ROUTE 1 - Fetch books and render them using the 'homepage' template
+app.get('/', async (req, res) => { 
+    const booksURL = 'https://api.hubapi.com/crm/v3/objects/2-134696617?limit=10&archived=false&properties=book_name,book_type,author,price';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json',
+    };
+    try {
+        const response = await axios.get(booksURL, { headers });
+        const books = response.data.results;
+        res.render('homepage', { title: 'Books | HubSpot APIs', books });
+    } catch (error) {
+        console.error('Error fetching books:', error.response?.data || error.message);
+        res.status(500).send('An error occurred while fetching books.');
+    }
+});
 
-// * Code for Route 1 goes here
+// Route to render the create book form
+app.get('/create', (req, res) => {
+    res.render('create', { title: 'Add New Book' });
+});
+
+// POST route to handle new book creation
+app.post('/create', async (req, res) => {
+    const newBook = {
+        properties: {
+            "book_name": req.body.book_name,
+            "book_type": req.body.book_type,
+            "author": req.body.author,
+            "price": req.body.price,
+        },
+    };
+
+    const createBookURL = 'https://api.hubapi.com/crm/v3/objects/2-134696617';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json',
+    };
+
+    try {
+        await axios.post(createBookURL, newBook, { headers });
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('An error occurred while creating the book.');
+    }
+});
+
+// GET route to render the update book form
+app.get('/update', async (req, res) => {
+    const id = req.query.id;
+    const getBookURL = `https://api.hubapi.com/crm/v3/objects/2-134696617/${id}?properties=book_name,book_type,author,price`;
+    const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json',
+    };
+  
+    try {
+      const response = await axios.get(getBookURL, { headers });
+      const book = response.data;
+      res.render('update', { title: 'Update Book', book });
+    } catch (error) {
+      console.error('Error fetching book:', error.response?.data || error.message);
+      res.status(500).send('An error occurred while fetching the book.');
+    }
+});
+
+// POST route to handle book updates
+app.post('/update', async (req, res) => {
+    const update = {
+      properties: {
+        "book_name": req.body.book_name,
+        "book_type": req.body.book_type,
+        "author": req.body.author,
+        "price": req.body.price,
+      },
+    };
+  
+    const id = req.body.hs_object_id;
+    const updateBookURL = `https://api.hubapi.com/crm/v3/objects/2-134696617/${id}`;
+    const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json',
+    };
+  
+    try {
+      await axios.patch(updateBookURL, update, { headers });
+      res.redirect('/');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('An error occurred while updating the book.');
+    }
+});
+
+/*
+
+// ROUTE 1 - Fetch contacts and render them using the 'contacts' template
+app.get('/contacts', async (req, res) => {
+  const contactsURL = 'https://api.hubapi.com/crm/v3/objects/contacts?limit=10&archived=false';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json',
+  };
+  // https://app.hubspot.com/contacts/145537472/objects/2-134696617/views/all/list
+  try {
+    const response = await axios.get(contactsURL, { headers });
+    const contacts = response.data.results;
+    //res.json(response.data.results)
+    res.render('contacts', { title: 'contacts | HubSpot APIs', contacts });
+  } catch (error) {
+    console.error('Error fetching contacts:', error.response?.data || error.message);
+    res.status(500).send('An error occurred while fetching contacts.');
+  }
+});
+
+
+app.get('/', async (req, res) => {
+  const contactsURL = 'https://api.hubapi.com/crm/v3/objects/contacts?limit=10&archived=false';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json',
+  };
+  // https://app.hubspot.com/contacts/145537472/objects/2-134696617/views/all/list
+  try {
+    const response = await axios.get(contactsURL, { headers });
+    const contacts = response.data.results;
+    //res.json(response.data.results)
+    res.render('contacts', { title: 'contacts | HubSpot APIs', contacts });
+  } catch (error) {
+    console.error('Error fetching contacts:', error.response?.data || error.message);
+    res.status(500).send('An error occurred while fetching contacts.');
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
+app.get('/update-cobj', async (req, res) => {
     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
@@ -41,8 +163,9 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+
+app.post('/update-cobj', async (req, res) => {
     const update = {
         properties: {
             "favorite_book": req.body.newVal
@@ -64,8 +187,15 @@ app.post('/update', async (req, res) => {
     }
 
 });
+
 */
-
-
 // * Localhost
+
+
+// Define a route handler for the default home page
+/*
+app.get('/', (req, res) => {
+    res.send('Hello Bachot Cholina');
+  });
+  */
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
